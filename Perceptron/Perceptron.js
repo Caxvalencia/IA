807673b45c186c444ef274f203cf9c48d04bcced
Cristian Alexis Salazar Valencia <caxvalencia@gmail.com>
2015-08-12 22:47:17 -0500

    Cambio de estructura
    - Se puso como librería base el javascript que se encuentra en la
    carpeta Percetron
    - Se renombro la palabra percetron por perceptron en todo el proyecto

---
+++ Perceptron/Perceptron.js
@@ -0,0 +1,101 @@
+var Perceptron = ( function( ProcesadorSinaptico ) {
+	/**
+	 * @construtor
+	 */
+	function Perceptron() {
+		this.rangoPesos = { MIN: -5, MAX: 4.9 };
+		this.hasError = false;
+		this.procesadorSinaptico = [];
+
+		this._pesos = null;
+		this._funcBack = function() {};
+	}
+
+	/**
+	 * Metodos privados
+	 */
+	function asignarPesos() {
+		var len = this.procesadorSinaptico[ 0 ].datos.length,
+			pesos = new Array( len ),
+			rango = this.rangoPesos.MAX - this.rangoPesos.MIN,
+			i;
+
+		for( i = 0; i < len; i++ ) {
+			while( !pesos[ i ] )
+				pesos[ i ] = parseFloat( ( Math.random() * rango + this.rangoPesos.MIN ).toFixed( 4 ) );
+		}
+
+		this.pesos( pesos );
+		this._funcBack( this );
+	};
+
+	/**
+	 * Metodos publicos
+	 */
+	Perceptron.prototype.addDatos = function( datos, salida ) {
+		if( datos[ 0 ] !== undefined )
+			if( datos[ 0 ][ 0 ] !== undefined ) {
+				for( var i = 0; i <= datos.length; i++ ) {
+					this.procesadorSinaptico.push( new ProcesadorSinaptico( datos[ i ], salida[ i ] ) );
+				}
+			} else
+				this.procesadorSinaptico.push( new ProcesadorSinaptico( datos, salida ) );
+		return this;
+	};
+
+	Perceptron.prototype.aprender = function() {
+		if( this.procesadorSinaptico.length === 0 )
+			return;
+
+		if( !this._pesos )
+			asignarPesos.call( this );
+
+		var procesadorSinaptico = null,
+			len = this.procesadorSinaptico.length,
+			i;
+		
+		this.hasError = false;
+
+		for( i = 0; i < len; i++ ) {
+			procesadorSinaptico = this.procesadorSinaptico[ i ];
+
+			procesadorSinaptico.calcularSinapsis( this._pesos );
+			procesadorSinaptico.calcularError();
+
+			if( procesadorSinaptico.error !== 0 ) {
+				this.hasError = true;
+				procesadorSinaptico.reajustarPesos( this._pesos );
+				this._funcBack( this );
+			}
+		}
+
+		if( this.hasError )
+			return this.aprender();
+	};
+
+	Perceptron.prototype.procesar = function( datos ) {
+		var procesadorSinaptico = new ProcesadorSinaptico( datos, null );
+		procesadorSinaptico.calcularSinapsis( this._pesos );
+
+		return procesadorSinaptico.salida();
+	};
+
+	Perceptron.prototype.funcBack = function( funcBack ) {
+		this._funcBack = funcBack || function() {};
+		return this;
+	};
+
+	/**
+	 * Getters y setters
+	 */
+	Perceptron.prototype.pesos = function( pesosSinapticos ) {
+		if( pesosSinapticos ) {
+			this._pesos = pesosSinapticos;
+			return this;
+		}
+
+		return this._pesos;
+	};
+
+	return Perceptron;
+})( ProcesadorSinaptico );
---
+++ Perceptron1/Perceptron.js
@@ -0,0 +1,123 @@
+var Perceptron = ( function() {
+	/**
+	 * @construtor
+	 */
+	function Perceptron() {
+		this.factorAprendizaje = 0.5;
+		this.rangoPesos = { MIN: -5, MAX: 4.9 };
+		this.pesos = null;
+		this.info = [];
+		this.hasError = false;
+	}
+
+	/**
+	 * Metodos privados
+	 */
+	Perceptron.prototype.salidaObtenida = function( sinapsis ) {
+		return sinapsis >= 0 ? 1 : 0;
+	};
+
+	Perceptron.prototype.asignarPesos = function() {
+		this.pesos = new Array( this.info[ 0 ].datos.length );
+		
+		var len = this.pesos.length, i,
+			rango = this.rangoPesos.MAX - this.rangoPesos.MIN;
+
+		for( i = 0; i < len; i++ ) {
+			while( !this.pesos[ i ] )
+				this.pesos[ i ] = parseFloat( ( Math.random() * rango + this.rangoPesos.MIN ).toFixed( 4 ) );
+		}
+	};
+
+	Perceptron.prototype.reajustarPesos = function( info ) {
+		var len = this.pesos.length, i;
+		
+		for( i = 0; i < len; i++ ) {
+			this.pesos[ i ] = this.pesos[ i ] + this.factorAprendizaje * info.error * info.datos[ i ];
+		}
+	};
+
+	Perceptron.prototype.calcularSinapsis = function( info ) {
+		var len = this.pesos.length, i;
+		info.sinapsis = 0;
+
+		for( i = 0; i < len; i++ ) {
+			info.sinapsis += info.datos[ i ] * this.pesos[ i ];
+		}
+
+		return this;
+	};
+
+	Perceptron.prototype.calcularError = function( info ) {
+		info.error = info.salida - this.salidaObtenida( info.sinapsis );
+		return this;
+	};
+
+	/**
+	 * Metodos publicos
+	 */
+	Perceptron.prototype.addDatos = function( datos, salida ) {
+		datos = [ 1 ].concat( datos );
+
+		this.info.push({
+			'datos': datos,
+			'salida': salida,
+			'sinapsis': 0,
+			'error': 0
+		});
+
+		return this;
+	};
+
+	Perceptron.prototype.aprender = function() {
+		if( this.info.length === 0 )
+			return;
+
+		if( !this.pesos )
+			this.asignarPesos();
+
+		var info = null, i,
+			len = this.info.length;
+		
+		this.hasError = false;
+
+		for( i = 0; i < len; i++ ) {
+			info = this.info[ i ];
+			this.calcularSinapsis( info );
+			this.calcularError( info );
+
+			if( info.error !== 0 ) {
+				this.hasError = true;
+				this.reajustarPesos( info );
+			}
+		}
+
+		if( this.hasError )
+			return this.aprender();
+	};
+
+	Perceptron.prototype.procesar = function( datos ) {
+		datos = [ 1 ].concat( datos );
+
+		var info = {
+			'datos': datos,
+			'salida': null,
+			'sinapsis': 0,
+			'error': 0
+		};
+
+		this.calcularSinapsis( info );
+		this.calcularError( info );
+
+		return this.salidaObtenida( info.sinapsis );
+	};
+
+	/**
+	 * Getters y setters
+	 */
+	Perceptron.prototype.getPesos = function() {
+		return this.pesos;
+	};
+
+	return Perceptron;
+})();
