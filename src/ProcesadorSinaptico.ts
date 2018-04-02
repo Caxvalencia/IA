5291b77d645692d1c143432433ea7017267859ac
Caxvalencia <caxvalencia@gmail.com>
2018-04-01 22:47:36 -0500

    Refactor to ProcesadorSinaptico class

--- src/ProcesadorSinaptico.ts
+++ src/ProcesadorSinaptico.ts
@@ -11,37 +11,35 @@ export class ProcesadorSinaptico {
         this.factorAprendizaje = 0.5;
         this.umbral = 1;
 
-        this.datos = datos ? [this.umbral].concat(datos) : [this.umbral];
         this.salidaDeseada = salidaDeseada;
         this.sinapsis = 0;
         this.error = 0;
         this.funcionActivacion = funcionActivacion;
+
+        this.setDatos(datos);        
     }
 
     salida() {
-        if (!this.funcionActivacion) return this.sinapsis >= 0 ? 1 : 0;
+        if (!this.funcionActivacion) {
+            return this.sinapsis >= 0 ? 1 : 0;
+        }
 
         //Funcion de activacion sigmoidal binaria
-        if (this.funcionActivacion === 'sigmoidal')
+        if (this.funcionActivacion === 'sigmoidal') {
             return 1 / (1 + Math.pow(Math.E, -this.sinapsis));
+        }
     }
 
     reajustarPesos(pesos) {
-        var len = pesos.length,
-            i;
-
-        for (i = 0; i < len; i++) {
-            pesos[i] =
-                pesos[i] + this.factorAprendizaje * this.error * this.datos[i];
+        for (let i = 0; i < pesos.length; i++) {
+            pesos[i] += this.factorAprendizaje * this.error * this.datos[i];
         }
     }
 
     calcularSinapsis(pesos) {
-        var len = pesos.length,
-            i;
         this.sinapsis = 0;
 
-        for (i = 0; i < len; i++) {
+        for (let i = 0; i < pesos.length; i++) {
             this.sinapsis += this.datos[i] * pesos[i];
         }
 
@@ -50,19 +48,19 @@ export class ProcesadorSinaptico {
 
     calcularError() {
         this.error = this.salidaDeseada - this.salida();
+
         return this;
     }
 
-    /**
-     * Getters y setters
-     */
     setDatos(datos) {
-        this.datos = datos;
+        this.datos = datos ? [this.umbral].concat(datos) : [this.umbral];
+
         return this;
     }
 
     setSalidaDeseada(salidaDeseada) {
         this.salidaDeseada = salidaDeseada;
+
         return this;
     }
 }
