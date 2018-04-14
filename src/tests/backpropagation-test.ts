a7a691036e9dfc4dce344208b05c808736798cf3
Caxvalencia <caxvalencia@gmail.com>
2018-04-14 18:02:34 -0500

    Add new tests and refactor for the test of backpropagation class

--- src/tests/backpropagation-test.ts
+++ src/tests/backpropagation-test.ts
@@ -7,27 +7,32 @@ import { Backpropagation } from '../backpropagation';
 export class PerceptronTest {
     @test
     public testXOR() {
-        var XOR = [
+        var data = [
             { input: [0, 0], output: 0 },
             { input: [0, 1], output: 1 },
             { input: [1, 0], output: 1 },
             { input: [1, 1], output: 0 }
         ];
 
-        var func_XOR = new Backpropagation();
-
-        func_XOR
-            .addLayer(2) // Entrada o primer capa oculta
+        var XOR = new Backpropagation();
+        XOR.addLayer(2) // Entrada o primer capa oculta
             .addLayer(1) // Salida o ultima capa
-            .learn(XOR);
+            .learn(data);
 
-        var results = [
-            func_XOR.process([0, 0])[0],
-            func_XOR.process([0, 1])[0],
-            func_XOR.process([1, 0])[0],
-            func_XOR.process([1, 1])[0]
-        ];
+        [
+            // Datas
+            [[0, 0], 0],
+            [[0, 1], 1],
+            [[1, 0], 1],
+            [[1, 1], 0]
+        ].forEach(dataForTest => {
+            let output = XOR.process(dataForTest[0])[0];
 
-        assert.isTrue(results[0] === 0, [0, 0] + ' -> ' + results[0]);
+            assert.equal(
+                output,
+                dataForTest[1],
+                dataForTest[0] + ' -> ' + output
+            );
+        });
     }
 }
