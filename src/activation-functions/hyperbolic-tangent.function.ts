6f7759bc959c428f9440af7d60a04d1836f50cd6
Caxvalencia <caxvalencia@gmail.com>
2018-05-28 23:50:47 -0500

    Add derivate function to activations function classes

--- src/activation-functions/hyperbolic-tangent.function.ts
+++ src/activation-functions/hyperbolic-tangent.function.ts
@@ -1,3 +1,9 @@
 export function hyperbolicTangent(synapse) {
     return Math.tanh(synapse);
 }
+
+export function prime(synapse) {
+    const output = hyperbolicTangent(synapse);
+
+    return 1.0 - output * output;
+}
--- src/activation-functions/sigmoidal.function.ts
+++ src/activation-functions/sigmoidal.function.ts
@@ -1,3 +1,9 @@
 export function sigmoidal(synapse) {
     return 1 / (1 + Math.pow(Math.E, -synapse));
 }
+
+export function prime(synapse) {
+    const output = sigmoidal(synapse);
+
+    return output - output * output;
+}
